605111cc23743bc1f8916d311867c2ff10306fb4
unknown <sigurezyx@gmail.com>
2024-11-18 18:18:21 +0800

    fix train.py

--- train.py
+++ train.py
@@ -12,22 +12,18 @@ from Utils.dataset import FPDataset
 from Utils.evaluate import validation
 from Utils.preprocess import RandomRotate90Degree
 
-#os.environ['CUDA_VISIBLE_DEVICES'] = '2, 3'
-
 def get_args():
     parser = argparse.ArgumentParser(description='Train the model on images')
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=60, help='Number of epochs')
     parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=32, help='Batch size')
     parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=5e-4, help='Learning rate', dest='lr')
-    parser.add_argument('--load', '-m', type=str, default=False, help='Load model from a .pth file')
     parser.add_argument('--validation', '-v', dest='val', type=float, default=0.1, help='Percent of the data that is used as validation (0-100)')
-    parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
     parser.add_argument('--size', '-s', type=int, default=224, help='Size of the images after preprocess', dest='size')
     parser.add_argument('--lambda', '-w', type=float, default=0.2, help="The weight of the Auxiliary Classifier's loss",dest='factor')
     parser.add_argument('--numval', '-n', type=int, default=2, help="The number of validation round in each epoch", dest='num_val')
+    parser.add_argument('--scheduler', '-o', type=bool, default=True, help="Enable learning rate scheduler", dest='scheduler')
     return parser.parse_args()
 
-#dir_img = '/home/share/dataset/training'
 dir_img = './data/training/'
 dir_checkpoint = './checkpoints'
 
@@ -38,7 +34,6 @@ def train_model(
         batch_size,
         learning_rate,
         val_percent,
-        amp,
         img_size,
         factor,
         save_checkpoint: bool = True
@@ -46,8 +41,6 @@ def train_model(
     # Create Dataset
     transform = transforms.Compose([
         transforms.RandomHorizontalFlip(p=0.5),
-        # transforms.RandomVerticalFlip(p=0.5),
-        #transforms.RandomRotation(90),
         RandomRotate90Degree()
     ])
     dataset = FPDataset(dir_img, img_size = img_size)
@@ -71,17 +64,17 @@ def train_model(
             Validation size: {n_val}
             Checkpoints:     {save_checkpoint}
             Device:          {device.type}
-            Mixed Precision: {amp}
             Images size:     {img_size}
             loss lambda:     {factor}
         ''')
 
-    # Set up the optimizer, the loss and the learning rate
+    # Set up the optimizer and the loss
     optimizer = optim.AdamW(model.parameters(), learning_rate)
-    # grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=6, factor = 0.5,
-                                                     threshold = 0.01, min_lr = 1e-6)
     criterion = nn.CrossEntropyLoss()
+
+    # Learning rate scheduler
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=10, factor=0.9,
+                                                     threshold=0.01, min_lr=1e-5)
 
     # Begin training
     global_step = 0
@@ -101,12 +94,8 @@ def train_model(
                 images = images.to(device=device, dtype=torch.float32)
                 labels = labels.to(device=device, dtype=torch.long)
                 optimizer.zero_grad(set_to_none=True)
-                # with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
-                #     main_logits, auxiliary_logits = model(images)
-                #     main_loss = criterion(main_logits, labels)
-                #     sub_loss = criterion(auxiliary_logits, labels)
-                #     loss = main_loss + factor * sub_loss
-                # Loss
+
+                # loss function
                 main_logits, auxiliary_logits = model(images)
                 main_loss = criterion(main_logits, labels)
                 sub_loss = criterion(auxiliary_logits, labels)
@@ -118,13 +107,10 @@ def train_model(
                 correct += torch.eq(pred, labels).sum().item()
                 train_acc = correct / total
 
-                #grad_scaler.scale(loss).backward()
+                # optimize
                 loss.backward()
                 optimizer.step()
                 global_step += 1
-                #grad_scaler.unscale_(optimizer)
-                #grad_scaler.step(optimizer)
-                #grad_scaler.update()
 
                 current_lr = optimizer.param_groups[0]['lr']
                 pbar.update(images.shape[0])
@@ -135,7 +121,8 @@ def train_model(
                 if total >= division_step or total == n_train:
                     division_step += division_step
                     acc = validation(model, val_loader, device)
-                    scheduler.step(acc)
+                    if args.scheduler:
+                        scheduler.step(acc)
                     final_accuracy = acc
                     if best_acc == 0:
                         best_acc = acc
@@ -163,11 +150,6 @@ if __name__ == '__main__':
     logging.info(f'Using device {device}')
 
     model = FPLDNet(input_size = args.size, patch_size = 16, embed_dim = 32, num_heads = 4)
-    if args.load:
-        state_dict = torch.load(args.load, map_location=device)
-        model.load_state_dict(state_dict)
-        logging.info(f'Model loaded from {args.load}')
-
     model = model.to(device)
 
     torch.cuda.empty_cache()
@@ -178,7 +160,6 @@ if __name__ == '__main__':
         batch_size = args.batch_size,
         learning_rate = args.lr,
         val_percent = args.val,
-        amp = args.amp,
         img_size = args.size,
         factor = args.factor
         )
